8e1fdded87a8b8fd4b046c2788f73131cac42794
Sara <EDIT@10.0.2.15>
2017-07-21 14:14:20 +0200

    Slashes deleted for GetMapping

--- src/main/java/com/teamshort/viberbot/mvc/controllers/administration/DefaultController.java
+++ src/main/java/com/teamshort/viberbot/mvc/controllers/administration/DefaultController.java
@@ -2,46 +2,48 @@ package com.teamshort.viberbot.mvc.controllers.administration;
 
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+//@EnableAutoConfiguration
 public class DefaultController {
 	
 
     @GetMapping("/")
     public String home1() {
-        return "/home";
+        return "home";
     }
 
     @GetMapping("/home")
     public String home() {
-        return "/home";
+        return "home";
     }
 
     @GetMapping("/admin")
     public String admin() {
-        return "/admin";
+        return "admin";
     }
 
     @GetMapping("/user")
     public String user() {
-        return "/user";
+        return "user";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "/about";
+        return "about";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
     }
 
     @GetMapping("/403")
     public String error403() {
-        return "/error/403";
+        return "error/403";
     }
     
     
