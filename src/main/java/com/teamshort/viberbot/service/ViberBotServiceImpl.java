16108791b2f5bc3c3905b1df48a32a965c58aa1e
jovana93nikolic <jovana993nikolic@gmail.com>
2017-07-25 12:31:22 +0200

    Keyboard

--- src/main/java/com/teamshort/viberbot/service/ViberBotServiceImpl.java
+++ src/main/java/com/teamshort/viberbot/service/ViberBotServiceImpl.java
@@ -12,6 +12,9 @@ import com.viber.bot.event.incoming.IncomingUnsubscribeEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
@@ -25,13 +28,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import com.google.common.io.CharStreams;
 import com.google.common.util.concurrent.Futures;
 import com.teamshort.viberbot.database.entity.User;
+import com.viber.bot.message.MessageKeyboard;
+//import com.teamshort.viberbot.message.MessageKeyboard;
 import com.viber.bot.message.TextMessage;
 import com.viber.bot.profile.UserProfile;
+
+import aj.org.objectweb.asm.TypeReference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Service
 public class ViberBotServiceImpl implements ViberBotService {
@@ -88,14 +101,153 @@ public class ViberBotServiceImpl implements ViberBotService {
 			
 		}));
 		
-		
-		
 	}
+	
+	
+	/*String keyboardMessageStr = {
+	"keyboard": {
+	"DefaultHeight": true,
+	"BgColor": "#FFFFFF",
+	"Buttons": [{
+	"Columns": 6,
+	"Rows": 1,
+	"BgColor": "#2db9b9",
+	"BgMediaType": "gif",
+	"BgMedia": "http://www.url.by/test.gif",
+	"BgLoop": true,
+	"ActionType": "open-url",
+	"ActionBody": "www.tut.by",
+	"Image": "www.tut.by/img.jpg",
+	"Text": "Key text",
+	"TextVAlign": "middle",
+	"TextHAlign": "center",
+	"TextOpacity": 60,
+	"TextSize": "regular"
+	}]
+	}
+	}*/
+	
+	
+	public final static String JSON_DATA =
+		     "{" 
+		   + "  \"Type\":\"keyboard\":"
+		   + "	\"DefaultHeight\":\"true\"," 
+		   + "	\"BgColor\":\"#FFFFFF\"," 
+		   + "  \"Buttons\": ["
+		   + "    {" 
+		   + "      \"Columns\": \"3\"," 
+		   + "      \"Rows\": \"2\","                  
+		   + "      \"BgColor\" : \"#2db9b9\"," 
+		   + "      \"BgMediaType\" : \"picture\"," 
+		   + "      \"BgMedia\" : \"http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg\"," 
+		   + "      \"BgLoop\": \"true\"," 
+		   + "      \"ActionType\": \"reply\"," 
+		   + "      \"ActionBody\": \"Yes\"," 
+		   //+ "      \"Image\" : \"www.tut.by/img.jpg\"," 
+		   //+ "      \"Text\" : \"Key text\"," 
+		   //+ "      \"TextVAlign\" : \"middle\","
+		   //+ "      \"TextHAlign\" : \"center\","
+		   //+ "      \"TextOpacity\" : \"60\","
+		   //+ "      \"TextSize\" : \"regular\""
+		   + "    }" 
+		   + "  ]" 
+		   + "}";
 
     @Override
 	public void onConversationStarted(ViberBot bot) {
-		bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of( // send 'Hi UserName' when conversation
-																				// is started
-				new TextMessage("Hello  ,  " + event.getUser().getName() + "! Welcome to ViberBot Room Reservation" ) )));
-	}
-}
+    	
+		//bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of
+    	//(/*new TextMessage("Hello  ,  " + event.getUser().getName() + 
+    	//"! Welcome to ViberBot Room Reservation" )*/)));
+    	
+    	String s =  "{" 
+    			   + "  \"Type\":\"keyboard\":"
+    			   + "	\"DefaultHeight\":\"true\"," 
+    			   + "	\"BgColor\":\"#FFFFFF\"," 
+    			   + "  \"Buttons\": ["
+    			   + "    {" 
+    			   + "      \"Columns\": \"3\"," 
+    			   + "      \"Rows\": \"2\","                  
+    			   + "      \"BgColor\" : \"#2db9b9\"," 
+    			   + "      \"BgMediaType\" : \"picture\"," 
+    			   + "      \"BgMedia\" : \"http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg\"," 
+    			   + "      \"BgLoop\": \"true\"," 
+    			   + "      \"ActionType\": \"reply\"," 
+    			   + "      \"ActionBody\": \"Yes\"," 
+    			   //+ "      \"Image\" : \"www.tut.by/img.jpg\"," 
+    			   //+ "      \"Text\" : \"Key text\"," 
+    			   //+ "      \"TextVAlign\" : \"middle\","
+    			   //+ "      \"TextHAlign\" : \"center\","
+    			   //+ "      \"TextOpacity\" : \"60\","
+    			   //+ "      \"TextSize\" : \"regular\""
+    			   + "    }" 
+    			   + "  ]" 
+    			   + "}";
+    	
+    	Map<String, Object> button1  = new HashMap<>();
+    	//button1.put("Type", "keyboard");
+    	button1.put("Columns", "2");
+    	button1.put("Rows", "1");
+    	button1.put("BgColor", "#2db9b9");
+    	button1.put("BgMediaType", "picture");
+    	button1.put("BgMedia", "http://www.url.by/test.gif");
+    	button1.put("BgLoop", "true");
+    	button1.put("ActionType", "open-url");
+    	button1.put("ActionBody", "www.tut.by");
+    	button1.put("Image", "www.tut.by/img.jpg");
+    	button1.put("Text", "Prvi button");
+    	button1.put("TextVAlign", "middle");
+    	button1.put("TextHAlign", "center");
+    	button1.put("TextOpacity", "60");
+    	button1.put("TextSize", "regular");
+    	
+    	Map<String, Object> button2  = new HashMap<>();
+    	//button2.put("Type", "keyboard");
+    	button2.put("Columns", "2");
+    	button2.put("Rows", "1");
+    	button2.put("BgColor", "#2db9b9");
+    	button2.put("BgMediaType", "picture");
+    	button2.put("BgMedia", "http://www.url.by/test.gif");
+    	button2.put("BgLoop", "true");
+    	button2.put("ActionType", "open-url");
+    	button2.put("ActionBody", "www.tut.by");
+    	button2.put("Image", "www.tut.by/img.jpg");
+    	button2.put("Text", "Prvi button");
+    	button2.put("TextVAlign", "middle");
+    	button2.put("TextHAlign", "center");
+    	button2.put("TextOpacity", "60");
+    	button2.put("TextSize", "regular");
+    	
+    	ArrayList<Map> button12  = new ArrayList<>();
+    	button12.add(button1);
+    	button12.add(button2);
+    	
+    	Map<String, Object> buttons  = new HashMap<>();
+    	buttons.put("Buttons", button12);
+    	
+    	Map<String, Object> height  = new HashMap<>();
+    	height.put("DefaultHeight", "regular");
+    	
+    	Map<String, Object> col  = new HashMap<>();
+    	col.put("BgColor", "#FFFFFF");
+    	
+    	Map<String, Object> keyboard  = new HashMap<>();
+    	keyboard.put("Buttons", button12);
+    	keyboard.put("DefaultHeight", "regular");
+    	keyboard.put("BgColor", "#FFFFFF");
+    	
+    	
+    	bot.onConversationStarted(event -> Futures.immediateFuture(Optional.of
+    	(new TextMessage("Hello  ,  " 
+    	+ event.getUser().getName()
+    	+ "! Welcome to ViberBot Room Reservation" 
+    	, new MessageKeyboard(keyboard), null, new Integer(1)
+    	
+    	
+    	
+    	)))); 				
+				
+		}
+			
+    }
+
