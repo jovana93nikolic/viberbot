71781397fc6a7e0b9a106682a1a706bf95f5459d
Sara <EDIT@10.0.2.15>
2017-07-26 15:35:51 +0200

    roomID changed into room_id in ReservationRepository

--- src/main/java/com/teamshort/viberbot/database/repository/ReservationRepository.java
+++ src/main/java/com/teamshort/viberbot/database/repository/ReservationRepository.java
@@ -11,8 +11,8 @@ import com.teamshort.viberbot.database.entity.Reservation;
 
 public interface ReservationRepository extends CrudRepository<Reservation, Long>{
 
-	@Query("select time from Reservation where room = :roomId and date = :date")
-	public Iterable<LocalTime> getFreeRoomCapacitiesOnDate(@Param("roomId") Long roomId, @Param("date") LocalDate date);
+	@Query("select time from Reservation where room = :room_id and date = :date")
+	public Iterable<LocalTime> getFreeRoomCapacitiesOnDate(@Param("room_id") Long roomId, @Param("date") LocalDate date);
 	
 	@Query("select r from Reservation r where r.userId = (select u.id from User u where u.viberId = :viberId)")
 	public Reservation getByUser(@Param("viberId") String viberId);
